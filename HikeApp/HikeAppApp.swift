efec826f95e0f2c7ba58f5da0d87bd67976f6c96
Iuliia Vlkv <yu.wlkw@gmail.com>
2024-05-16 19:38:49 +0500

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Iuliia Volkova on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
