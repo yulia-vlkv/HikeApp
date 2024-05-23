e55e8494a22e1681e74546d18c62d24322423b15
Iuliia Vlkv <yu.wlkw@gmail.com>
2024-05-23 20:47:25 +0500

    Added a new animation

---
+++ HikeApp/Component/MotionAnimationView.swift
@@ -0,0 +1,74 @@
+//
+//  MotionAnimationView.swift
+//  HikeApp
+//
+//  Created by Iuliia Volkova on 23.05.2024.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: - PROPERTIES
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    @State private var isAnumating: Bool = false
+    
+    // MARK: - FUNCTIONS
+    //1. Random Coordinate
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    
+    //2. Random Size
+    func randomSize() -> CGFloat {
+        return CGFloat.random(in: 4...80)
+    }
+    
+    //3. RandomScale
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    
+    //4. Random Speed
+    func randonmSpeed() -> Double {
+        return Double.random(in: 0.05...1)
+    }
+    
+    //5. Random Delay
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    var body: some View {
+        ZStack {
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .frame(width: randomSize())
+                    .position(
+                        x: randomCoordinate(),
+                        y: randomCoordinate()
+                    )
+                    .scaleEffect(isAnumating ? randomScale() : 1)
+                    .onAppear(perform: {
+                        withAnimation(
+                            .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                            .speed(randonmSpeed())
+                            .delay(randomDelay())
+                        ) {
+                            isAnumating = true
+                        }
+                    })
+            }
+        } //: ZSTACK
+        .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup()
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+        .background(Circle().fill(.teal))
+}
