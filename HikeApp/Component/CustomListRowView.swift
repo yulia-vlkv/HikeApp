1b1877f83be45d0474c9c96e3d7f7c95efdfc875
Iuliia Vlkv <yu.wlkw@gmail.com>
2024-07-12 19:58:47 +0500

    Added settings, a link

--- HikeApp/Component/CustomListRowView.swift
+++ HikeApp/Component/CustomListRowView.swift
@@ -12,15 +12,25 @@ struct CustomListRowView: View {
     // MARK: - PROPERTIES
     @State var rowLabel: String
     @State var rowIcon: String
-    @State var rowContent: String
+    @State var rowContent: String? = nil
     @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
     
     var body: some View {
         LabeledContent{
             //Content
-            Text(rowContent)
-                .foregroundColor(.primary)
-                .fontWeight(.heavy)
+            if rowContent != nil {
+                Text(rowContent!)
+                    .foregroundColor(.primary)
+                    .fontWeight(.heavy)
+            } else if (rowLinkLabel != nil) && (rowLinkDestination != nil) {
+                Link(rowLinkLabel!, destination: URL(string: rowLinkDestination!)!)
+                    .foregroundColor(.pink)
+                    .fontWeight(.heavy)
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            }
         } label: {
             // Label
             HStack {
--- HikeApp/Screen/SettingsView.swift
+++ HikeApp/Screen/SettingsView.swift
@@ -72,7 +72,20 @@ struct SettingsView: View {
                 // LabeledContent("Application", value: "Hike")
                 
                 // 2. Advanced labeled content
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintColor: .blue)
                 
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
+                
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Iuliia", rowTintColor: .mint)
+                
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+                
+    
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Acedemy", rowLinkDestination: "https://credo.academy")
                 
             } //: SECTION
         } //: LIST
